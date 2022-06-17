c005a24351a52c2f506f93de4e40ce8f6eabf803
Vladimir Dadyka <dadyka.vova@gmail.com>
2022-06-17 16:59:32 +0300

    imported fonts, made title

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,33 @@
-<template></template>
+<template>
+  <div class="container">
+    <h1 class="app__title">Ты сегодня покормил кота?</h1>
+    <ul class="app__cats">
+      <li class="app__cat"></li>
+    </ul>
+  </div>
+</template>
 
 <script>
 export default {};
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Exo+2:wght@100&family=Roboto&display=swap");
+body {
+  font-family: "Exo 2", sans-serif;
+  background-image: url("assets/pattern.jpg");
+}
+.container {
+  max-width: 1120px;
+  margin: 0 auto;
+}
+.app {
+  &__title {
+    padding-top: 36px;
+    text-align: center;
+    font-size: 36px;
+    line-height: 44px;
+    color: #fff;
+  }
+}
+</style>
