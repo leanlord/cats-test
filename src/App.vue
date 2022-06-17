1f1d378199152acd8f72d75e78e63f60e9e956fe
Vladimir Dadyka <dadyka.vova@gmail.com>
2022-06-17 17:28:21 +0300

    made cat component and logic

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,38 @@
   <div class="container">
     <h1 class="app__title">Ты сегодня покормил кота?</h1>
     <ul class="app__cats">
-      <li class="app__cat"></li>
+      <li class="app__cat" v-for="cat in catsExample" :key="cat">
+        <FeedCat
+          :filling="cat.filling"
+          :category="cat.category"
+          :mouse="cat.mouse"
+          :portions="cat.portions"
+          :weight="cat.weight"
+        ></FeedCat>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
-export default {};
+import FeedCat from "@/components/FeedCat";
+export default {
+  components: { FeedCat },
+  setup() {
+    const catsExample = [
+      {
+        category: "Сказочное заморское яство",
+        filling: "фуа-гура",
+        portions: 10,
+        mouse: 3,
+        weight: 0.5,
+      },
+    ];
+    return {
+      catsExample,
+    };
+  },
+};
 </script>
 
 <style lang="scss">
@@ -28,6 +53,10 @@ body {
     font-size: 36px;
     line-height: 44px;
     color: #fff;
+  }
+  &__cats {
+    display: flex;
+    justify-content: space-between;
   }
 }
 </style>
---
+++ src/components/FeedCat.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="cat">
+    <p class="cat__category">
+      {{ category }}
+    </p>
+    <div class="cat__head">
+      <p class="cat__title">Нямушка</p>
+      <p class="cat__subtitle">с {{ filling }}</p>
+    </div>
+    <div class="cat__portions">
+      <p class="cat__additional">{{ portions }} порций</p>
+      <p class="cat__gift">
+        {{ mouse }} {{ mouse === 1 ? "мышь" : mouse < 5 ? "мыши" : "мышей" }} в
+        подарок
+      </p>
+    </div>
+    <div class="cat__weight">{{ weight }} кг</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FeedCat",
+  props: ["category", "filling", "portions", "weight", "mouse"],
+};
+</script>
+
+<style scoped lang="scss">
+.cat {
+  width: 320px;
+  background-color: #f2f2f2;
+  background-image: url("../assets/cat.jpg");
+}
+</style>
